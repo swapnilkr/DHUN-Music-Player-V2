8b25ae2de3b2ff999abe7cc27da194c843575915
swapnilkr099 <swapnilkr099@gmail.com>
2023-11-11 19:32:21 +0530

    Set up tsa

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Image from 'next/image';
 import NavBar from './navbar';
+import TSA from './tsa';
 
 export default function Home() {
     return (
         <>
             <NavBar />
+            <main>
+                <aside className    ="aside section-1">
+                    <TSA />
+                </aside>
+            </main>
+
         </>
     )
 
---
+++ app/tsa.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+
+
+export default function TSA() {
+    return (
+        <>
+            <div className="outer-carousel">
+        		<div className="carousel">
+
+                    <img src="/crousel/carousel1.jpg" />
+                    <img src="/crousel/carousel3.jpg" />
+                    <img src="/crousel/carousel2.jpg" />
+                </div>
+            </div>
+        </>
+    )
+
+}
