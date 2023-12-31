23b771b2ea33e93ccb0b414b4a8a17ac106dad66
swapnilkr099 <swapnilkr099@gmail.com>
2023-12-31 15:43:38 +0530

    CSS configure

--- app/Album/albumDesc.tsx
+++ app/Album/albumDesc.tsx
@@ -2,6 +2,35 @@ function AlbumDesc() {
 
     return(
         <>
+            <div className="jumbotron" >
+            	 <img src="/Jumbtron back.png" />
+                <div className="jumbotron-image">
+                    <img src="/coldplay back.jpg" />
+                </div>
+                <div className="jumbotron-detail">
+                    <div className="descriptive-part">
+                        <div className="song-name-main">
+                            <h1>ColdPlay</h1>
+                        </div>
+                        <div className="followers">
+                            17.5M Followers
+                        </div>
+                        <p className="author">British Band</p>
+                        <p className="desc">
+                            Coldplay are a British rock band that were formed in London in 1996. Vocalist and pianist Chris Martin, guitarist Jonny Buckland, bassist Guy Berryman, and drummer Will Champion met at University College London and began playing music together from 1996 to 1998, first calling themselves Pectoralz and then Starfish before finally changing their name to Coldplay.
+                        </p>
+                    </div>
+                    <div className="action-part">
+                        <div className="btns">
+                            <button>Play All</button>
+                            <button>Follow</button>
+                        </div>
+                        <p>
+                            250 Tracks | 147 Albums
+                        </p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 
