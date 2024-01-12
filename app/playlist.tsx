c0f330577ac4953d7c4de20b3b159705ddf4d9f8
swapnilkr099 <swapnilkr099@gmail.com>
2024-01-12 09:02:08 +0530

    Feature: Added Queue options

--- app/playlist.tsx
+++ app/playlist.tsx
@@ -19,13 +19,13 @@ function Playlist() {
             <div className="heading">
                 <h1> Playlist </h1>
                 <h4>
-                    <div className='queue' onMouseOver={(event)=> showDropdownOnClick(event)} onMouseOut={(event)=> showDropdownOnClick(event)} onClick={(event) => showDropdownOnClick(event)}>CLICK <i className="fas fa-chevron-circle-down"></i></div>
+                    <div className='queue' onMouseOver={(event)=> showDropdownOnClick(event)} onBlur={(event)=> showDropdownOnClick(event)} onClick={(event) => showDropdownOnClick(event)}>CLICK <i className="fas fa-chevron-circle-down"></i></div>
                 </h4>
             </div>
             {
                 showDropdown ?
                 
-                <div className="queue-options" id="queue-option-box">
+                <div onMouseOver={(e) =>setShowDropdown(true)} onMouseOut={(e) =>setShowDropdown(false)}  className="queue-options" id="queue-option-box">
                     <p><div style={{color:"#f8f9fa", fontWeight:"bold", cursor:"pointer"}} onClick={(event) => showPlayList(event)}>Playlists</div></p>
                     <hr />
                     <p><div style={{color:"#f8f9fa", fontWeight:"bold", cursor:"pointer"}} onClick={(event) => showFavList(event)}>Favourite songs</div></p>
